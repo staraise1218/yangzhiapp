f4fb12822ab17a4243dbb0358232e8d6cb5f1eeb
jiqishoubi <437753880@qq.com>
2018-12-25 20:27:21 +0800

    分享

--- application/mobile/view/static/js/pig_detail.js
+++ application/mobile/view/static/js/pig_detail.js
@@ -37,17 +37,47 @@ var ZhuDetail = { //猪场id pig_id
         $(".pigImg").attr("src", Global.domain + info.thumb)
         $(".pigImg").show()
       }
+
+      //分享 剪切板
+      ZhuDetail.initshare()
     }
   },
   initshare() {
-    let url = window.location.href
-    $(".share").attr("data-clipboard-text", url)
+    let shareStr = $(".container1 .title").text() + "-养殖app " + window.location.href
+    $(".share").attr("data-clipboard-text", shareStr)
     var clipboard = new ClipboardJS('.share');
 
     clipboard.on('success', function (e) {
       console.info('Action:', e.action);
       console.info('Text:', e.text);
       console.info('Trigger:', e.trigger);
+      //生成分享弹框
+      let $msgDiv = $(`
+            <div class="msgWrap">
+                <div class="msgMask"></div>
+                <div class="msg">
+                    <div class="">
+                      <h4>分享内容已生成</h4>
+                      <p>${shareStr}</p>
+                      <p>内容已复制成功</p>
+                    </div>
+                    <p class="msgCtrl">
+                        <span class="closeMsg">去粘贴</span>
+                    </p>
+                </div>
+            </div>
+      `);
+      $msgDiv.find(".msgMask")[0].addEventListener("touchstart", function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+      }, false);
+      $msgDiv.find(".closeMsg").click(function () {
+        $msgDiv.remove();
+        if (callback) {
+          callback()
+        }
+      });
+      $("body").append($msgDiv);
 
       e.clearSelection();
     });
@@ -58,10 +88,10 @@ var ZhuDetail = { //猪场id pig_id
     });
   },
   eventBind() {
-    //点击分享?
-    $(".share").click(function () {
+    // //点击分享?
+    // $(".share").click(function () {
 
-    })
+    // })
     //点击收藏
     $(".collection").click(function () {
       Global.collect(this, {
@@ -78,8 +108,6 @@ var ZhuDetail = { //猪场id pig_id
     let option = Global.getPageParams()
     ZhuDetail.id = option.id ? Number(option.id) : ""
     // ZhuDetail.id = 1 //测试
-    //分享 剪切板
-    ZhuDetail.initshare()
     ZhuDetail.getInfo(function () {
       ZhuDetail.updateDom()
     })
