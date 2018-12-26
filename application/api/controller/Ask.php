4ce44540c8a766fe3ef5fed6adbfd7d9af8ca0bb
shengli <363912809@qq.com>
2018-12-26 17:45:10 +0800

    bug

--- application/api/controller/Ask.php
+++ application/api/controller/Ask.php
@@ -88,7 +88,7 @@ class Ask extends Base {
         $info = Db::name('ask')->alias('a')
             ->join('users u', 'a.user_id=u.user_id', 'left')
             ->where('id', $id)
-            ->field('u.user_id, u.head_pic, u.fullname, a.expert_id, a.content, a.images, a.createtime')
+            ->field('u.user_id, u.head_pic, u.fullname, a.expert_id, a.title, a.content, a.images, a.createtime')
             ->find();
 
         if($info && $info['images'] != '') $info['images'] = json_decode($info['images']);
